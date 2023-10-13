43566f3ccd50f3f310651869efe46594b119570d
Leonard Niehaus <niehaus.1301@gmail.com>
2023-10-13 16:32:12 +0200

    fix ts error

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,14 +1,17 @@
 import { ReactTerminal } from "react-terminal";
 import commands from "./commands";
 
-export default function App(props) {
+export default function App() {
   // Define commands here
 
   return (
-    <ReactTerminal
-      commands={commands}
-      theme="material-dark"
-      welcomeMessage={commands.help}
-    />
+    <div style={{ height: "90vh", display: "flex", flexDirection: "column" }}>
+      <ReactTerminal
+        style={{ flex: 1 }}
+        commands={commands}
+        theme="material-dark"
+        welcomeMessage={commands.help}
+      />
+    </div>
   );
 }
