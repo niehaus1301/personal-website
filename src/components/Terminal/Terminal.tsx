0a0d76633d08880b1150340eec2ca826aa5b3170
Leonard Niehaus <niehaus.1301@gmail.com>
2023-10-13 18:14:11 +0200

    add better error message

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -5,13 +5,16 @@ export default function App() {
   // Define commands here
 
   return (
-    <div style={{ height: "90vh", display: "flex", flexDirection: "column" }}>
-      <ReactTerminal
-        style={{ flex: 1 }}
-        commands={commands}
-        theme="material-dark"
-        welcomeMessage={commands.help}
-      />
-    </div>
+    <ReactTerminal
+      commands={commands}
+      theme="material-dark"
+      welcomeMessage={commands.help}
+      errorMessage={
+        <span>
+          Command not found! Type "<strong>help</strong>" to see a list of
+          available commands.
+        </span>
+      }
+    />
   );
 }
