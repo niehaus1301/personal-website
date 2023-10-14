00eab50e8d7c88c5cc6614b3e2774fa0cef30e07
Leonard Niehaus <niehaus.1301@gmail.com>
2023-10-14 16:46:50 +0200

    move welcome message to Terminal Component

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,14 +1,23 @@
 import { ReactTerminal } from "react-terminal";
 import commands from "./commands";
+import calculateAge from "../../utils/calulcateAge";
 
 export default function App() {
-  // Define commands here
+  const yearsOld = calculateAge(new Date("2001-07-13"));
 
   return (
     <ReactTerminal
       commands={commands}
       theme="material-dark"
-      welcomeMessage={commands.help}
+      welcomeMessage={
+        <span>
+          Welcome to resume-cli version {yearsOld}-sinceborn developed by
+          @niehaus1301. <br />
+          To get started, enter one of the following commands and press enter:
+          <br />
+          {commands.help}
+        </span>
+      }
       errorMessage={
         <span>
           Command not found! Type "<strong>help</strong>" to see a list of
