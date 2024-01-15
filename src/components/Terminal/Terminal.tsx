b507e8fc2df24358be8769a10db056994d3428c7
Leonard <leonard.niehaus@airbus.com>
2024-01-15 14:21:53 +0100

    add terminal context into terminal

--- src/components/Terminal/Terminal.tsx
+++ src/components/Terminal/Terminal.tsx
@@ -1,4 +1,4 @@
-import { ReactTerminal } from "react-terminal";
+import { ReactTerminal, TerminalContextProvider } from "react-terminal";
 import commands from "./commands";
 import calculateAge from "./utils";
 import { CSSProperties } from "react";
@@ -12,27 +12,30 @@ export default function App({ exit, style }: Props) {
   const yearsOld = calculateAge(new Date("2001-07-13"));
 
   return (
-    <div style={{ ...(style || {}), height: "100%", width: "100%" }}>
-      <ReactTerminal
-        commands={{ ...commands, exit }}
-        theme="material-dark"
-        showControlBar={false}
-        welcomeMessage={
-          <span>
-            Welcome to resume-cli version {yearsOld}-sinceborn developed by
-            @niehaus1301. <br />
-            To get started, enter one of the following commands and press enter:
-            <br />
-            {commands.help}
-          </span>
-        }
-        errorMessage={
-          <span>
-            Command not found! Type "<strong>help</strong>" to see a list of
-            available commands.
-          </span>
-        }
-      />{" "}
-    </div>
+    <TerminalContextProvider>
+      <div style={{ ...(style || {}), height: "100%", width: "100%" }}>
+        <ReactTerminal
+          commands={{ ...commands, exit }}
+          theme="material-dark"
+          showControlBar={false}
+          welcomeMessage={
+            <span>
+              Welcome to resume-cli version {yearsOld}-sinceborn developed by
+              @niehaus1301. <br />
+              To get started, enter one of the following commands and press
+              enter:
+              <br />
+              {commands.help}
+            </span>
+          }
+          errorMessage={
+            <span>
+              Command not found! Type "<strong>help</strong>" to see a list of
+              available commands.
+            </span>
+          }
+        />
+      </div>
+    </TerminalContextProvider>
   );
 }
