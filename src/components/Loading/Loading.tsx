d018214452d787ce16a2720ae039ae5231e75a1b
Leonard Niehaus <niehaus.1301@gmail.com>
2024-01-31 14:49:18 +0100

    seperate loaders

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,23 @@
+import CircularProgress from "@mui/joy/CircularProgress";
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
+
+interface Props {
+  caption: string;
+}
+
+export default function Loading({ caption }: Props) {
+  return (
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100%"
+      width="100%"
+      spacing={1}
+    >
+      <CircularProgress />
+      <Typography level="h3">{caption}</Typography>
+    </Stack>
+  );
+}
---
+++ src/components/MainLoading/Loading.tsx
@@ -0,0 +1,32 @@
+import { LinearProgress, Typography } from "@mui/joy";
+import { Stack } from "@mui/material";
+
+export default function Loading() {
+  return (
+    <Stack
+      position="fixed"
+      zIndex={1}
+      top={0}
+      left={0}
+      width="100%"
+      height="100%"
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      sx={{ backgroundColor: "background.body" }}
+    >
+      <Typography level="h1" fontWeight="bold">
+        Leonard Niehaus
+      </Typography>
+      <LinearProgress
+        variant="soft"
+        size="lg"
+        sx={{ width: 200, maxHeight: 8 }}
+      />
+      <Typography level="body-md" fontStyle="italic">
+        Tidying up my room...
+      </Typography>
+    </Stack>
+  );
+}
