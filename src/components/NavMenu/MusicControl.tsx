04ffcb10a23bc76f67106a53b6d5f6e618432e92
Leonard Niehaus <niehaus.1301@gmail.com>
2024-02-03 18:18:13 +0100

    fix music stopping on menu close

--- src/components/NavMenu/MusicControl.tsx
+++ src/components/NavMenu/MusicControl.tsx
@@ -1,9 +1,8 @@
 import IconButton from "@mui/joy/IconButton";
 import { type Application } from "@splinetool/runtime";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import PlayArrowRoundedIcon from "@mui/icons-material/PlayArrowRounded";
 import StopRoundedIcon from "@mui/icons-material/StopRounded";
-import useTabVisibility from "@/hooks/useTabVisibility";
 
 interface Props {
   splineApp: Application;
@@ -14,35 +13,7 @@ export default function MusicControl({ splineApp }: Props) {
     splineApp.getVariable("musicPlaying") as boolean
   );
   const [audio] = useState(new Audio("music.mp3"));
-  const tabIsVisible = useTabVisibility();
-  const isPlayingRef = useRef(false);
-
   audio.loop = true;
-  audio.muted = !tabIsVisible;
-
-  useEffect(() => {
-    return () => {
-      // Clean up audio when the component is unmounted
-      audio.pause();
-      audio.currentTime = 0;
-    };
-  }, [audio]);
-
-  useEffect(() => {
-    // Update audio properties when tab visibility changes
-    audio.muted = !tabIsVisible;
-
-    // If music is supposed to be playing and it's not currently playing, start playing
-    if (musicPlaying && !isPlayingRef.current) {
-      audio.play().then(() => {
-        isPlayingRef.current = true;
-      });
-    } else if (!musicPlaying && isPlayingRef.current) {
-      // If music is supposed to be paused and it's currently playing, pause it
-      audio.pause();
-      isPlayingRef.current = false;
-    }
-  }, [musicPlaying, tabIsVisible, audio]);
 
   function toggle() {
     setMusicPlaying(!musicPlaying);
@@ -52,6 +23,17 @@ export default function MusicControl({ splineApp }: Props) {
   splineApp.addEventListener("mouseDown", ({ target }) => {
     if (target.name === "Radio") toggle();
   });
+
+  useEffect(() => {
+    musicPlaying ? audio.play() : audio.pause();
+  }, [musicPlaying, audio]);
+
+  useEffect(() => {
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, [audio]);
 
   return (
     <IconButton aria-label="Toggle Music" onClick={toggle}>
