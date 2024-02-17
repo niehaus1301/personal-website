e81954929d5fc19dcf41e5deb6ed91d1c120df18
Leonard Niehaus <niehaus.1301@gmail.com>
2024-02-17 15:06:48 +0100

    fix travelmap navigating always to landingpage

--- src/components/TravelMap/TravelMap.tsx
+++ src/components/TravelMap/TravelMap.tsx
@@ -54,7 +54,7 @@ export default function TravelMap() {
           width: "auto",
           zIndex: 1,
         }}
-        onClick={() => navigate("/")}
+        onClick={() => navigate(-1)}
       />
       {!mapLoaded && <Loading caption="Loading Map..." />}
       <div style={{ width: "100%", height: "100%" }} ref={mapContainer} />
