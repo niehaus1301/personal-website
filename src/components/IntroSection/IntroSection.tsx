a150138aa5e8fe61813f9f79fa6d63ee779f85f7
Leonard Niehaus <niehaus.1301@gmail.com>
2024-04-27 21:06:10 +0200

    make hand wave

--- src/components/IntroSection/IntroSection.tsx
+++ src/components/IntroSection/IntroSection.tsx
@@ -1,9 +1,12 @@
+import "./IntroSection.css";
 import { Stack, Typography } from "@mui/joy";
 
 export default function IntroSection() {
   return (
     <Stack spacing={2}>
-      <Typography level="h1">Hi, I'm Leonard 👋</Typography>
+      <Typography level="h1">
+        Hi, I'm Leonard <span className="wave">👋</span>
+      </Typography>
       <Typography level="body-lg">
         A full-stack developer from Germany 🇩🇪. <br />
         I'm currently working at Airbus to satisfy my curiousity for aviation ✈️
