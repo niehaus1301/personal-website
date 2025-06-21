7242d330f00f02a3e4944a09ef1de499992849e1
Leonard Niehaus <niehaus.1301@gmail.com>
2025-06-21 12:45:12 +0200

    add autaxo

--- src/components/ProjectsSection/ProjectChip.tsx
+++ src/components/ProjectsSection/ProjectChip.tsx
@@ -7,12 +7,16 @@ interface Props {
 
 export default function ProjectChip({ project }: Props) {
   return project.type === "personal" ? (
-    <Chip size="lg" color="warning">
+    <Chip size="lg" color="success">
       Personal Project
+    </Chip>
+  ) : project.type === "work" ? (
+    <Chip size="lg" color="warning">
+      Work Project @ {project.work}
     </Chip>
   ) : (
     <Chip size="lg" color="danger">
-      Work Project @ {project.work}
+      Startup
     </Chip>
   );
 }
